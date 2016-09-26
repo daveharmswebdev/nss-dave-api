600225c33591c1d57a05084d7308dbc13fadc06c
Dave Harms <daveharmswebdev@gmail.com>
2016-09-26 11:57:52 -0500

    added chat partial linked to mongodb

--- client/app/main.js
+++ client/app/main.js
@@ -8,6 +8,10 @@ angular
 				controller: 'main',
 				templateUrl: 'partials/main.html'
 			})
+			.when('/chat', {
+				controller: 'chat',
+				templateUrl: 'partials/chat.html'
+			})
 	)
 	.controller('main', function($scope, $http) {
 		$http
@@ -15,5 +19,13 @@ angular
 			.then(({data: { title }}) => {
 				console.log('title:', title)
 				$scope.name = title 
-		})
+			})
+	})
+	.controller('chat', function($scope, $http) {
+		$http
+			.get('/api/messages')
+			.then(({data: { messages }} ) => {
+					console.log('messages', messages)
+					$scope.messages = messages
+			}) 
 	})
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 'use strict'
 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
+
+const app = express()
 const port = process.env.PORT || 3000
 
 // arguable
@@ -11,9 +14,24 @@ app.set('port', port)
 app.use(express.static('client'))
 
 app.get('/api/title', (req,res) => {
-	res.json({ title: 'World!' })
+	res.json({ title: 'Mean Chat!' })
 })
 
-app.listen(port, () => {
-	console.log('now listening on port', port)
+const Message = mongoose.model('message', {
+	author: String,
+	message: String
 })
+
+app.get('/api/messages', (req, res, err) => {
+	Message
+		.find()
+		.then( messages => res.json({ messages }))
+		.catch(err)
+})
+
+mongoose.promise = Promise
+mongoose.connect(MONGODB_URL, () =>
+	app.listen(port, () => {
+		console.log('now listening on port', port)
+	})
+)
