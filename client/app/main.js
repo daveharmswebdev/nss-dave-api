81b3b6370c52d507ca1714af77dfc200c86a1fb8
Dave Harms <daveharmswebdev@gmail.com>
2016-09-26 11:10:32 -0500

    added router and partials

--- client/app/main.js
+++ client/app/main.js
@@ -1,12 +1,19 @@
 'use strict'
 
 angular
-	.module('app', [])
+	.module('app', ['ngRoute'])
+	.config($routeProvider => 
+		$routeProvider
+			.when('/', {
+				controller: 'main',
+				templateUrl: 'partials/main.html'
+			})
+	)
 	.controller('main', function($scope, $http) {
 		$http
 			.get('/api/title')
 			.then(({data: { title }}) => {
-				console.log('title', title)
+				console.log('title:', title)
 				$scope.name = title 
 		})
 	})
